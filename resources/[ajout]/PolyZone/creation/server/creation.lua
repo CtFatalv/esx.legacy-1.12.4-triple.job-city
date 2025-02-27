b969e3ac103e3f164abcfa410e88917dc0935d2b
CtFatalv <contact.fatalv@gmail.com>
2025-02-27 20:03:41 +0100

    Legacy City

--- resources/[ajout]/PolyZone/creation/server/creation.lua
+++ resources/[ajout]/PolyZone/creation/server/creation.lua
@@ -1,3 +1,7 @@
+local function round(num)
+  return string.format("%.2f", num)
+end
+
 RegisterNetEvent("polyzone:printPoly")
 AddEventHandler("polyzone:printPoly", function(zone)
   local fileName = "polyzone_created_zones.txt"
@@ -6,13 +10,13 @@ AddEventHandler("polyzone:printPoly", function(zone)
   local header = "-- Name: " .. zone.name .. " | " .. formattedDate
   local points = ""
   for _, point in ipairs(zone.points) do
-    points = points .. "  vector2(" .. point.x .. ", " .. point.y .. "),\n"
+    points = points .. "  vector2(" .. round(point.x) .. ", " .. round(point.y) .. "),\n"
   end
   local polyData = header .. "\n" ..
   "PolyZone:Create({\n" ..
   points ..
   "},)\n" ..
-  "---------------------------------------------------------\n"  -- Ajouter un séparateur après la zone
+  "---------------------------------------------------------\n"
   created_zones = created_zones .. polyData
   SaveResourceFile(GetCurrentResourceName(), fileName, created_zones, -1)
 end)
@@ -23,8 +27,8 @@ AddEventHandler("polyzone:printCircle", function(zone)
   local created_zones = LoadResourceFile(GetCurrentResourceName(), fileName) or ""
   local formattedDate = os.date("%Y-%m-%dT%H:%M:%S")
   local header = "-- Name: " .. zone.name .. " | " .. formattedDate
-  local center = "vector3(" .. zone.center.x .. ", " .. zone.center.y .. ", " .. zone.center.z .. ")"
-  local radius = zone.radius or 0
+  local center = "vector3(" .. round(zone.center.x) .. ", " .. round(zone.center.y) .. ", " .. round(zone.center.z) .. ")"
+  local radius = round(zone.radius or 0)
   local options = {
     "name = \"" .. zone.name .. "\"",
     "useZ = " .. (zone.useZ ~= nil and tostring(zone.useZ) or "true"),
@@ -35,7 +39,7 @@ AddEventHandler("polyzone:printCircle", function(zone)
   "CircleZone:Create(" .. center .. ", " .. radius .. ", {\n" ..
   "  " .. options_str .. "\n" ..
   "})\n" ..
-  "---------------------------------------------------------\n"  -- Ajouter un séparateur après la zone
+  "---------------------------------------------------------\n"
   created_zones = created_zones .. circleData
   SaveResourceFile(GetCurrentResourceName(), fileName, created_zones, -1)
 end)
@@ -46,19 +50,19 @@ AddEventHandler("polyzone:printBox", function(zone)
   local created_zones = LoadResourceFile(GetCurrentResourceName(), fileName) or ""
   local formattedDate = os.date("%Y-%m-%dT%H:%M:%S")
   local header = "-- Name: " .. zone.name .. " | " .. formattedDate
-  local minZ = zone.minZ
-  local maxZ = zone.maxZ
+  local minZ = zone.minZ and round(zone.minZ) or nil
+  local maxZ = zone.maxZ and round(zone.maxZ) or nil
   local minZ_str = minZ and "minZ = " .. minZ or "--minZ = aucun,"
   local maxZ_str = maxZ and "maxZ = " .. maxZ or "--maxZ = aucun,"
   local boxData = header .. "\n" ..
-  "BoxZone:Create(vector3(" .. zone.center.x .. ", " .. zone.center.y .. ", " .. zone.center.z .. "), " ..
-  zone.length .. ", " .. zone.width .. ", {\n" ..
+  "BoxZone:Create(vector3(" .. round(zone.center.x) .. ", " .. round(zone.center.y) .. ", " .. round(zone.center.z) .. "), " ..
+  round(zone.length) .. ", " .. round(zone.width) .. ", {\n" ..
   "  name = \"" .. zone.name .. "\",\n" ..
-  "  heading = " .. zone.heading .. ",\n" ..
+  "  heading = " .. round(zone.heading) .. ",\n" ..
   "  " .. minZ_str .. "\n" ..
   "  " .. maxZ_str .. "\n" ..
   "})\n" ..
-  "---------------------------------------------------------\n"  -- Ajouter un séparateur après la zone
+  "---------------------------------------------------------\n"
   created_zones = created_zones .. boxData
   SaveResourceFile(GetCurrentResourceName(), fileName, created_zones, -1)
 end)
